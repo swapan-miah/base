58f5ea0091353cdc47a48c5288b6830448202b1e
swapan-miah <swaponmiah120@gmail.com>
2023-06-18 16:43:45 +0600

    img galary commplete

--- main.js
+++ main.js
@@ -1,43 +1,28 @@
+const list = document.querySelectorAll(".list");
+const filterDiv = document.querySelectorAll(".filterDiv");
 
-filterSelection("all")
-function filterSelection(c) {
-  var x, i;
-  x = document.getElementsByClassName("filterDiv");
-  if (c == "all") c = "";
-  for (i = 0; i < x.length; i++) {
-    w3RemoveClass(x[i], "show");
-    if (x[i].className.indexOf(c) > -1) w3AddClass(x[i], "show");
-  }
-}
-
-function w3AddClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    if (arr1.indexOf(arr2[i]) == -1) {element.className += " " + arr2[i];}
-  }
-}
-
-function w3RemoveClass(element, name) {
-  var i, arr1, arr2;
-  arr1 = element.className.split(" ");
-  arr2 = name.split(" ");
-  for (i = 0; i < arr2.length; i++) {
-    while (arr1.indexOf(arr2[i]) > -1) {
-      arr1.splice(arr1.indexOf(arr2[i]), 1);     
+for (let i = 0; i < list.length; i++) {
+  list[i].addEventListener('click', function(){
+    // console.log("click")
+    for (let j = 0; j < list.length; j++) {
+      list[j].classList.remove("active");
     }
-  }
-  element.className = arr1.join(" ");
-}
+    this.classList.add("active")
 
-// Add active class to the current button (highlight it)
-var btnContainer = document.getElementById("myBtnContainer");
-var btns = btnContainer.getElementsByClassName("btn");
-for (var i = 0; i < btns.length; i++) {
-  btns[i].addEventListener("click", function(){
-    var current = document.getElementsByClassName("active");
-    current[0].className = current[0].className.replace(" active", "");
-    this.className += " active";
-  });
+    const dataFilter = this.getAttribute("date-filter");
+    console.log(dataFilter)
+    
+    for (let k = 0; k < filterDiv.length; k++) {
+      filterDiv[k].classList.remove("active1")
+      filterDiv[k].classList.add("hide")
+
+      if(filterDiv[k].getAttribute("data-item") == dataFilter || dataFilter == "all"){
+        filterDiv[i].classList.remove("hide");
+        filterDiv[i].classList.add("active");
+      }
+    }
+  })
+  
+  
+
 }
